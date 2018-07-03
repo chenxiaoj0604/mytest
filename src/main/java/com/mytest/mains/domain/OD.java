9166489c0f00a32e895ec3a9a324ff0247ddbfef
dx <604043033@qq.com>
2018-07-03 23:20:19 +0800

    importXLS

---
+++ src/main/java/com/mytest/mains/domain/OD.java
@@ -0,0 +1,36 @@
+package com.mytest.mains.domain;
+
+/**
+ * Created by duanxun on 2018/7/3.
+ */
+public class OD {
+
+    private int start1;
+
+    private int start2;
+
+    private int end1_1;
+
+    private int end1_2;
+
+    private int end1_3;
+
+    private int end1_4;
+
+    private int end1_5;
+
+    private int end1_6;
+
+    private int end2_1;
+
+    private int end2_2;
+
+    private int end2_3;
+
+    private int end2_4;
+
+    private int end2_5;
+
+    private int end2_6;
+
+}
---
+++ src/main/java/com/mytest/mains/importXLS.java
@@ -0,0 +1,60 @@
+package com.mytest.mains;
+
+import com.mytest.mains.domain.OD;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by duanxun on 2018/7/3.
+ */
+public class importXLS {
+    public static void main(String[] args) {
+        try {
+            InputStream is = new FileInputStream("src/main/java/com/mytest/mains/file/od.xls");
+            HSSFWorkbook hssfWorkbook = new HSSFWorkbook(is);
+            OD od = null;
+            List<OD> odList = new ArrayList<OD>();
+            // 循环工作表Sheet
+            for (int numSheet = 0; numSheet < hssfWorkbook.getNumberOfSheets(); numSheet++) {
+                HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
+                if (hssfSheet == null) {
+                    continue;
+                }
+                // 循环行Row
+                for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+                    HSSFRow hssfRow = hssfSheet.getRow(rowNum);
+                    if (hssfRow != null) {
+                        HSSFCell no = hssfRow.getCell(0);
+                        System.out.println(getValue(no));
+                    }
+                }
+            }
+            //todo
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("static-access")
+    private static String getValue(HSSFCell hssfCell) {
+        if (hssfCell.getCellType() == hssfCell.CELL_TYPE_BOOLEAN) {
+            // 返回布尔类型的值
+            return String.valueOf(hssfCell.getBooleanCellValue());
+        } else if (hssfCell.getCellType() == hssfCell.CELL_TYPE_NUMERIC) {
+            // 返回数值类型的值
+            return String.valueOf(hssfCell.getNumericCellValue());
+        } else {
+            // 返回字符串类型的值
+            return String.valueOf(hssfCell.getStringCellValue());
+        }
+    }
+}
+
